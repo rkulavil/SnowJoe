c92a7b354cd9a736222534d0e34b2c153bc88507
rkulavil <82693608+rkulavil@users.noreply.github.com>
2021-04-18 23:00:38 -0400

    Add files via upload

---
+++ SnowJoe/OrderManager.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SnowJoe
+{
+    public class OrderManager : IOrderManager
+    {
+        private readonly IOrder _order;
+        private readonly IDealItem _item;
+
+        public OrderManager(IOrder order, IDealItem dealItem)
+        {
+            _item = dealItem;
+            _order = order;
+        }
+
+        public List<Order> GetAllOrders()
+        {
+            return _order.Orders;
+        }
+
+        public List<DealItem> GetAllItems()
+        {
+            return _item.Items;
+        }
+        /// <summary>
+        /// Adds an item to the list of items.
+        /// </summary>
+        /// <param name="item">item to be added.</param>
+        public void AddItem(Item item)
+        {
+            try
+            {
+                if (!_item.Items.Any(x => x.ItemNumber == item.ItemNumber))
+                {
+                    _item.Items.Add(new DealItem { ItemNumber = item.ItemNumber, AvailableQuantity = item.AvailableQuantity, Price = item.Price });
+                    Console.WriteLine($"Item Number {item.ItemNumber} is added to the list of items.");
+                }
+                else
+                {
+                    Console.WriteLine("Duplicate ItemNumber. The item you are trying to add is already available in the list of items.");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.StackTrace);
+            }
+        }
+        /// <summary>
+        /// Removes an item from the list of items.
+        /// </summary>
+        /// <param name="itemNumber">Item Number representing the item to be deleted.</param>
+        public void RemoveItem(string itemNumber)
+        {
+            try
+            {
+                if (_item.Items.Count == 0)
+                {
+                    Console.WriteLine("The list of items is empty");
+                }
+                else if (_item.Items.Any(i => i.ItemNumber == itemNumber))
+                {
+                    _item.Items.RemoveAll(x => x.ItemNumber == itemNumber);
+                    _order.Orders.RemoveAll(o => o.ItemNumber == itemNumber);
+                    Console.WriteLine($"Item {itemNumber} has been removed from the list of items and corresponding orders have been cancelled.");
+                }
+                else
+                {
+                    Console.WriteLine($"Item Number {itemNumber} is invalid.");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.StackTrace);
+            }
+        }
+
+        /// <summary>
+        /// Adds an item to the list of deals.
+        /// </summary>
+        /// <param name="dealItem">item to be added as deal</param>
+        public void AddDeal(DealItem dealItem)
+        {
+            try
+            {
+                if (DateTime.Compare(dealItem.EndDate, dealItem.StartDate) < 0)
+                    Console.WriteLine("Deal Item's End Date has to be greater than Start Date.");
+                else if (!_item.Items.Any(di => di.ItemNumber == dealItem.ItemNumber))
+                {
+                    dealItem.IsDealItem = true;
+                    _item.Items.Add(dealItem);
+                    Console.WriteLine($"Item Number {dealItem.ItemNumber} is added to the list of deal items.");
+                }
+                else
+                    Console.WriteLine($"Deal already exists for Item Number: {dealItem.ItemNumber}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.StackTrace);
+            }
+        }
+
+        /// <summary>
+        /// Adds order to the list of orders.
+        /// </summary>
+        /// <param name="order">order to be added.</param>
+        public void AddOrder(Order order)
+        {
+            try
+            {
+                if (_item.Items.Any(i => i.ItemNumber == order.ItemNumber))
+                {
+                    if (_item.Items.Any(i => i.AvailableQuantity >= order.QuantityOrdered))
+                    {
+                        order.OrderId = Guid.NewGuid().ToString();
+                        _order.Orders.Add(order);
+                        _item.Items[_item.Items.FindIndex(i => i.ItemNumber == order.ItemNumber)].AvailableQuantity -= order.QuantityOrdered;
+                        Console.WriteLine($"Item Number {order.ItemNumber} is added to the list of orders.");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Item: {order.ItemNumber} is currently out of stock.");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine($"Item: {order.ItemNumber} is an invalid item.");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.StackTrace);
+            }
+        }
+
+        /// <summary>
+        /// Prints in a new line all items with their ItemNumber-AvailableQuantity-Price properties separated with dash.
+        /// </summary>
+        public void PrintItems()
+        {
+            try
+            {
+                if (_item.Items?.Count > 0)
+                {
+                    Console.WriteLine("The list of Items are (Item Number - Available Quantity - Price - Is Available in Deal?):");
+                    _item.Items.ForEach(item => Console.WriteLine($"{item.ItemNumber}-{item.AvailableQuantity}-{item.Price}-{(item.IsDealItem ? "Yes" : "No")}"));
+                }
+                else
+                    Console.WriteLine("The list of items is empty.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.StackTrace);
+            }
+        }
+
+        /// <summary>
+        /// Prints in a new line all orders with their properties separated with dash.
+        /// </summary>
+        public void PrintOrders()
+        {
+            try
+            {
+                if (_order.Orders?.Count > 0)
+                {
+                    Console.WriteLine("The list of Items are (Order Id - Date Created - Item Number - Quantity Ordered)");
+                    _order.Orders.ForEach(order => Console.WriteLine($"{order.OrderId}-{order.DateCreated}-{order.ItemNumber}-{order.QuantityOrdered}"));
+                }
+                else
+                    Console.WriteLine("The list of orders is empty.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.StackTrace);
+            }
+        }
+    }
+}
---
+++ SnowJoe/Program.cs
@@ -0,0 +1,150 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+
+namespace SnowJoe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var serviceCollection = new ServiceCollection();
+                ConfigureServices(serviceCollection);
+                var _serviceProvider = serviceCollection.BuildServiceProvider();
+                var orderManager = _serviceProvider.GetService<IOrderManager>();
+                var calc = _serviceProvider.GetService<ICalc>();
+                int choice = default(int);
+                var selection = true;
+                
+                ShowSelections();
+
+                while (selection)
+                {
+                    int.TryParse(Console.ReadLine(), out choice);
+
+                    switch (choice)
+                    {
+                        case 1:
+                            var newItem = new Item();
+                            Console.Write("Enter Item Number: ");
+                            newItem.ItemNumber = Console.ReadLine();
+                            Console.Write("Enter Available Quantity: ");
+                            newItem.AvailableQuantity = Convert.ToInt32(Console.ReadLine());
+                            Console.Write("Enter Price: ");
+                            newItem.Price = Convert.ToDecimal(Console.ReadLine());
+                            orderManager.AddItem(newItem);
+                            break;
+                        case 2:
+                            Console.Write("Enter Item Number: ");
+                            var itemNumber = Console.ReadLine();
+                            orderManager.RemoveItem(itemNumber);
+                            break;
+                        case 3:
+                            var newDeal = new DealItem();
+                            Console.Write("Enter Deal Item Number: ");
+                            newDeal.ItemNumber = Console.ReadLine();
+
+                            Console.Write("Enter Available Quantity: ");
+                            newDeal.AvailableQuantity = Convert.ToInt32(Console.ReadLine());
+
+                            Console.Write("Enter Price: ");
+                            newDeal.Price = Convert.ToDecimal(Console.ReadLine());
+
+                            Console.Write("Enter Start Date (MM/dd/yyyy): ");
+                            DateTime.TryParse(Console.ReadLine(), out DateTime startDate);
+                            newDeal.StartDate = startDate;
+
+                            Console.Write("Enter End Date (MM/dd/yyyy): ");
+                            DateTime.TryParse(Console.ReadLine(), out DateTime endDate);
+                            newDeal.EndDate = endDate;
+
+                            Console.Write("Enter Discount (percentage): ");
+                            newDeal.Discount = Convert.ToDecimal(Console.ReadLine());
+
+                            orderManager.AddDeal(newDeal);
+                            break;
+                        case 4:
+                            var order = new Order { DateCreated = DateTime.Now };
+
+                            Console.Write("Enter Item Number to Order: ");
+                            order.ItemNumber = Console.ReadLine();
+
+                            Console.Write("Enter Quantity: ");
+                            order.QuantityOrdered = Convert.ToInt32(Console.ReadLine());
+
+                            orderManager.AddOrder(order);
+                            break;
+                        case 5:
+                            orderManager.PrintItems();
+                            break;
+                        case 6:
+                            orderManager.PrintOrders();
+                            break;
+                        case 7:
+                            Console.Write("Enter Order Id: ");
+                            var orderId = Console.ReadLine();
+                            Console.Write("Enter Item Number: ");
+                            var itmNbr = Console.ReadLine();
+
+                            Console.WriteLine($"Profit made from Order Number - {orderId} with Item Number - {itmNbr} is { calc.GetProfit(orderId, itmNbr, false) }");
+                            break;
+                        case 8:
+                            Console.Write("Enter Order Id: ");
+                            var ordrId = Console.ReadLine();
+                            Console.Write("Enter Item Number: ");
+                            var itemNbr = Console.ReadLine();
+
+                            Console.WriteLine($"Profit made from Order Number - {ordrId} with Item Number - {itemNbr} is { calc.GetProfit(ordrId, itemNbr, true) }");
+                            break;
+                        case 9:
+                            Console.WriteLine($"Profit made from all the existing orders is { calc.GetProfitLoss() }");
+                            break;
+                        case 10:
+                            selection = false;
+                            break;
+                        default:
+                            Console.WriteLine("Choose a valid option from the menu.");
+                            break;
+                    }
+
+                    if (selection)
+                    {
+                        Console.WriteLine(Environment.NewLine);
+                        ShowSelections();
+                    }
+                }
+                Console.WriteLine("Process Exited");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.StackTrace);
+            }
+        }
+
+        private static void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSingleton<IOrder, Order>();
+            services.AddSingleton<IDealItem, DealItem>();
+            services.AddSingleton<IOrderManager, OrderManager>();
+            services.AddSingleton<ICalc, Calc>();
+        }
+
+        private static void ShowSelections()
+        {
+            Console.WriteLine("1. Add Item");
+            Console.WriteLine("2. Remove Item");
+            Console.WriteLine("3. Add Deal");
+            Console.WriteLine("4. Add Order");
+            Console.WriteLine("5. Print Items");
+            Console.WriteLine("6. Print Orders");
+            Console.WriteLine("7. Get Profit For An Item");
+            Console.WriteLine("8. Get Profit For A Deal Item");
+            Console.WriteLine("9. Get Profit For All Orders");
+            Console.WriteLine("10. Exit");
+            Console.Write("Select your option: ");
+        }
+    }
+}
